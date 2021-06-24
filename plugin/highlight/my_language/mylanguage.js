abe4917fed82677a10f8cece45f0f67176bcdbbf
Robin Schwendele <rnsd@gft.com>
2021-06-24 12:28:40 +0200

    final praesi

--- plugin/highlight/my_language/mylanguage.js
+++ plugin/highlight/my_language/mylanguage.js
@@ -1,26 +1,17 @@
 
 var module = module ? module : {};     // shim for browser use
 
-function hljsDefineTerraform(hljs) {
-	var NUMBERS = {
-		className: 'number',
-		begin: '\\b\\d+(\\.\\d+)?',
-		relevance: 0
+function hljsDefineMyLanguage(hljs) {
+
+	var FOO = {
+		className: 'foo',
+		begin: 'foo',
+		relevance: 1
 	};
 
     return {
     	aliases: ['ml'],
-    	keywords: 'my language|10',
-    	literal: 'foo bar',
-    	contains: [
-       		hljs.COMMENT('\\#', '$'),
-       		NUMBERS
-    	]
+    	keywords: ['my', 'language', '10'],
+    	contains: [ FOO ]
     }
 }
-
-module.exports = function(hljs) {
-    hljs.registerLanguage('mylanguage', hljsDefineMyLanguage);
-};
-
-module.exports.definer = hljsDefineMyLanguage;
